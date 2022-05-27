4611e050cdec4133d0a941f0a727ed2f6e5be50c
mariano <marianotorresleyva@gmail.com>
2022-05-27 14:08:43 -0400

    Commit to change of branch

---
+++ src/figure.js
@@ -0,0 +1,11 @@
+class Figure {
+    #piezas = 4;
+    constructor({ color, nombre }) {
+        this.color = color;
+        this.nombre = nombre;
+    }
+
+    moveY() {}
+    moveX() {}
+    rotate() {}
+}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-document.addEventListener("load", () => {
+window.addEventListener("load", () => {
     (function canvas(numeroDeElemetos) {
         let section = document.querySelector("section");
-        for (let i = 0; i <= numeroDeElemetos; i++) {
+        for (let i = 0; i < numeroDeElemetos; i++) {
             let canvasBlock = document.createElement("span");
             canvasBlock.className = "canvas-block";
             section.appendChild(canvasBlock);
