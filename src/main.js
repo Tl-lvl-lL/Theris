d666d5af09317f54a8c302b2cf2dd5bef81c9996
mariano <marianotorresleyva@gmail.com>
2022-05-26 23:12:11 -0400

    begging again & putting the bases

---
+++ src/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("load", () => {
+    (function canvas(numeroDeElemetos) {
+        let section = document.querySelector("section");
+        for (let i = 0; i <= numeroDeElemetos; i++) {
+            let canvasBlock = document.createElement("span");
+            canvasBlock.className = "canvas-block";
+            section.appendChild(canvasBlock);
+        }
+    })(100);
+});
